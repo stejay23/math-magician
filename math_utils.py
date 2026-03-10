94e28883b8819545fe787b004f402d0429ef8822
ziemaa23 <ziemaa23@htl-kaindorf.at>
2026-03-10 12:03:31 +0100

    Add simple subtraction function

---
+++ math_utils.py
@@ -0,0 +1,56 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# subtraction function
+def subtract(a, b):
+    """
+    Returns the subtraction of b from a
+    """
+    return a - b
+
+
+# simple test (optional)
+if __name__ == "__main__":
+    print(subtract(10, 5))
